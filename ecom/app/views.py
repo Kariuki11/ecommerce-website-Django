deac7b6de89daa8e124d43b47a02043924865b77
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-11 14:17:45 +0300

    changes

--- ecom/app/views.py
+++ ecom/app/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.views import View
 from . models import Product
 
-
 #Create your views here.
 def home(request):
   return render (request, "app/home.html")
@@ -24,23 +23,4 @@ class CategoryView(View):
 
 
 
-# from django.db.models import Count
-# from django.shortcuts import render
-# from django.views import View
-# from .models import Product
-
-# class CategoryView(View):
-#     def get(self, request, val):
-#         # Query the Product model
-#         products = Product.objects.filter(category=val)
-        
-#         # Calculate title counts
-#         title_counts = products.values('title').annotate(total=Count('title'))
-        
-#         # Pass variables to the template
-#         context = {
-#             'val': val,
-#             'title_counts': title_counts
-#         }
-#         return render(request, "app/category.html", context)
 
