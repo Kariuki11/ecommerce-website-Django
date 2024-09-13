d45c3012b2677850ba780b2c5fa9cf28fdf43f9f
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-13 12:15:04 +0300

    "Added new URL pattern for category title and corresponding view in views.py"

--- ecom/app/views.py
+++ ecom/app/views.py
@@ -16,6 +16,7 @@ class CategoryView(View):
 class CategoryTitle(View):
   def get(self, request, val):
     product = Product.objects.filter(title=val)
+    title =product.objects.filter(category=product[0].category).values('title')
     return render(request, "app/category.html", locals())
 class ProductDetail(View):
     def get(self,request,pk):
